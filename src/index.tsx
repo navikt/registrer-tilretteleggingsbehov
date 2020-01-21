d4017a9e9d15344fcbdcaa9369186a7d15de7c1b
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-01-21 09:53:32 +0100

    Eksporter app med NAVSPA

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NAVSPA from '@navikt/navspa';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('oppfolging-tilretteleggingsbehov-utvikling'));
+if (document.getElementById('oppfolging-tilretteleggingsbehov-utvikling')) {
+    ReactDOM.render(<App />, document.getElementById('oppfolging-tilretteleggingsbehov-utvikling'));
+} else {
+    NAVSPA.eksporter('tilretteleggingsbehov', App);
+}
