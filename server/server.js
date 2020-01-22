b5dc88fbf72344e662e3620d3a15500c6668ddc8
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-01-22 10:34:56 +0100

    Sett 'evig' caching på server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,13 @@ const BASE_PATH = '/registrer-tilretteleggingsbehov';
 const buildPath = path.join(__dirname, '../build');
 
 const startServer = () => {
-    app.use(BASE_PATH, express.static(buildPath));
+    app.use(
+        BASE_PATH,
+        express.static(buildPath, {
+            immutable: true,
+            maxAge: 365000000,
+        })
+    );
 
     app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
