113a2a91703667ceb838e958ea9f84d6c7e8a492
André Mossige <mossige.a@gmail.com>
2020-01-23 16:10:34 +0100

    Set no-cache

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const BASE_PATH = '/registrer-tilretteleggingsbehov';
 const buildPath = path.join(__dirname, '../build');
 
 const startServer = () => {
-    app.use(BASE_PATH, express.static(buildPath));
+    app.use(BASE_PATH, express.static(buildPath), {
+        setHeaders: res => {
+            res.set('Cache-Control', 'no-cache');
+        },
+    });
 
     app.get(`${BASE_PATH}/internal/isAlive`, (req, res) => res.sendStatus(200));
     app.get(`${BASE_PATH}/internal/isReady`, (req, res) => res.sendStatus(200));
