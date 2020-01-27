6d309f7d0bb607c03ae8b23eebf58878563cf070
André Mossige <mossige.a@gmail.com>
2020-01-27 15:34:16 +0100

    Slå på nav font globalt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import Registrering from './registrering/Registrering';
 import Visning from './visning/Visning';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 export enum Visningstype {
     VisTilretteleggingsbehov = 'VIS_TILRETTELEGGINGSBEHOV',
@@ -13,13 +14,16 @@ interface Props {
 }
 
 const App: FunctionComponent<Props> = ({ viewType, fnr }) => {
+    let side;
     if (viewType === Visningstype.RegistrerTilretteleggingsbehov) {
-        return <Registrering fnr={fnr} />;
+        side = <Registrering fnr={fnr} />;
     } else if (viewType === Visningstype.VisTilretteleggingsbehov) {
-        return <Visning fnr={fnr} />;
+        side = <Visning fnr={fnr} />;
     } else {
         return null;
     }
+
+    return <Normaltekst>{side}</Normaltekst>;
 };
 
 export default App;
