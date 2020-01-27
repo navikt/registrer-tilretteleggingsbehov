d428b57732fa3c1e9dff1f5189bc3f603a88f02f
André Mossige <mossige.a@gmail.com>
2020-01-27 10:51:24 +0100

    Flytt alternativ til egne variabler

--- src/registrering/alternativ.ts
+++ src/registrering/alternativ.ts
@@ -16,85 +16,90 @@ export type Alternativ = {
 export const hentAlternativer = (kategori: Kategori): Alternativ[] => {
     switch (kategori) {
         case 'arbeidstid':
-            return [
-                { behov: ArbeidstidBehov.KanIkkeJobbe, label: 'Kan ikke jobbe nå' },
-                { behov: ArbeidstidBehov.Heltid, label: 'Heltid' },
-                {
-                    behov: ArbeidstidBehov.IkkeHeleDager,
-                    label: 'Kan jobbe hver ukedag, men ikke hele dager',
-                },
-                {
-                    behov: ArbeidstidBehov.BorteFasteDagerEllerTider,
-                    label: 'Må være borte fra jobben til faste dager eller tider',
-                },
-                { behov: ArbeidstidBehov.Fleksibel, label: 'Må ha fleksible arbeidsdager' },
-            ];
+            return arbeidstid;
 
         case 'fysisk':
-            return [
-                {
-                    behov: FysiskBehov.Arbeidsstilling,
-                    label: 'Varierte arbeidsstillinger',
-                },
-                {
-                    behov: FysiskBehov.Ergonomi,
-                    label: 'Ergonomiske tilpasninger',
-                    hjelpetekst:
-                        'For eksempel hev/senk-pult eller tilpassede lys- eller lydforhold',
-                },
-                { behov: FysiskBehov.TungeLøft, label: 'Unngå tunge løft' },
-                { behov: FysiskBehov.Hørsel, label: 'Hørsel' },
-                { behov: FysiskBehov.Syn, label: 'Syn' },
-                {
-                    behov: FysiskBehov.AndreFormer,
-                    label: 'Andre former for fysisk tilrettelegging',
-                },
-            ];
+            return fysisk;
 
         case 'arbeidsmiljø':
-            return [
-                {
-                    behov: ArbeidsmijøBehov.TilrettelagtOpplæring,
-                    label: 'Tilrettelagt opplæring',
-                    hjelpetekst:
-                        'For eksempel hyppige tilbakemeldinger eller lengre opplæringsperiode',
-                },
-                {
-                    behov: ArbeidsmijøBehov.TilrettelagteArbeidsoppgaver,
-                    label: 'Tilrettelagte arbeidsoppgaver',
-                    hjelpetekst:
-                        'For eksempel tydelige oppgaver eller unntak fra noen typer oppgaver',
-                },
-                {
-                    behov: ArbeidsmijøBehov.Mentor,
-                    label: 'Mentor',
-                    hjelpetekst: 'En egen person med ansvar for tett oppfølging',
-                },
-                { behov: ArbeidsmijøBehov.Annet, label: 'Andre former for tilrettelegging' },
-            ];
+            return arbeidsmiljø;
 
         case 'grunnleggende':
-            return [
-                {
-                    behov: GrunnleggendeBehov.SnakkeNorsk,
-                    label: 'Snakke norsk',
-                },
-                {
-                    behov: GrunnleggendeBehov.SkriveNorsk,
-                    label: 'Skrive norsk',
-                },
-                {
-                    behov: GrunnleggendeBehov.LeseNorsk,
-                    label: 'Lese norsk',
-                },
-                {
-                    behov: GrunnleggendeBehov.RegningOgTallforståelse,
-                    label: 'Regning og tallforståelse',
-                },
-                { behov: GrunnleggendeBehov.AndreUtfordringer, label: 'Andre utfordringer' },
-            ];
+            return grunnleggende;
 
         default:
             return [];
     }
 };
+
+const arbeidstid = [
+    { behov: ArbeidstidBehov.KanIkkeJobbe, label: 'Kan ikke jobbe nå' },
+    { behov: ArbeidstidBehov.Heltid, label: 'Heltid' },
+    {
+        behov: ArbeidstidBehov.IkkeHeleDager,
+        label: 'Kan jobbe hver ukedag, men ikke hele dager',
+    },
+    {
+        behov: ArbeidstidBehov.BorteFasteDagerEllerTider,
+        label: 'Må være borte fra jobben til faste dager eller tider',
+    },
+    { behov: ArbeidstidBehov.Fleksibel, label: 'Må ha fleksible arbeidsdager' },
+];
+
+const fysisk = [
+    {
+        behov: FysiskBehov.Arbeidsstilling,
+        label: 'Varierte arbeidsstillinger',
+    },
+    {
+        behov: FysiskBehov.Ergonomi,
+        label: 'Ergonomiske tilpasninger',
+        hjelpetekst: 'For eksempel hev/senk-pult eller tilpassede lys- eller lydforhold',
+    },
+    { behov: FysiskBehov.TungeLøft, label: 'Unngå tunge løft' },
+    { behov: FysiskBehov.Hørsel, label: 'Hørsel' },
+    { behov: FysiskBehov.Syn, label: 'Syn' },
+    {
+        behov: FysiskBehov.AndreFormer,
+        label: 'Andre former for fysisk tilrettelegging',
+    },
+];
+
+const arbeidsmiljø = [
+    {
+        behov: ArbeidsmijøBehov.TilrettelagtOpplæring,
+        label: 'Tilrettelagt opplæring',
+        hjelpetekst: 'For eksempel hyppige tilbakemeldinger eller lengre opplæringsperiode',
+    },
+    {
+        behov: ArbeidsmijøBehov.TilrettelagteArbeidsoppgaver,
+        label: 'Tilrettelagte arbeidsoppgaver',
+        hjelpetekst: 'For eksempel tydelige oppgaver eller unntak fra noen typer oppgaver',
+    },
+    {
+        behov: ArbeidsmijøBehov.Mentor,
+        label: 'Mentor',
+        hjelpetekst: 'En egen person med ansvar for tett oppfølging',
+    },
+    { behov: ArbeidsmijøBehov.Annet, label: 'Andre former for tilrettelegging' },
+];
+
+const grunnleggende = [
+    {
+        behov: GrunnleggendeBehov.SnakkeNorsk,
+        label: 'Snakke norsk',
+    },
+    {
+        behov: GrunnleggendeBehov.SkriveNorsk,
+        label: 'Skrive norsk',
+    },
+    {
+        behov: GrunnleggendeBehov.LeseNorsk,
+        label: 'Lese norsk',
+    },
+    {
+        behov: GrunnleggendeBehov.RegningOgTallforståelse,
+        label: 'Regning og tallforståelse',
+    },
+    { behov: GrunnleggendeBehov.AndreUtfordringer, label: 'Andre utfordringer' },
+];
