b83dde3853cf946deb7c0b5707df131c52deef80
Joar Aurdal <joar.aurdal@gmail.com>
2020-01-29 14:13:35 +0100

    mapping behovtekster (#10)
    * mapping behovtekster
    
    * liste for arbeidstid
    
    * endret til hentBehovtekster
    
    * Flytt behovtekster inn i domenefilen
    
    Co-authored-by: André Mossige <mossige.a@gmail.com>

--- src/mock/testdata.ts
+++ src/mock/testdata.ts
@@ -1,14 +1,15 @@
 import { ArbeidstidBehov, FysiskBehov, GrunnleggendeBehov } from '../api/Behov';
+import { Kandidat } from '../api/Kandidat';
 
 export const etFnr = '12345678901';
 
-export const enKandidat = {
+export const enKandidat: Kandidat = {
     aktørId: '4943247287476',
     fnr: etFnr,
     sistEndret: '2019-09-22T20:38:53.312Z',
     sistEndretAv: 'J337738',
     navKontor: '1001',
-    arbeidstidBehov: ArbeidstidBehov.IkkeHeleDager,
+    arbeidstidBehov: [ArbeidstidBehov.IkkeHeleDager, ArbeidstidBehov.Fleksibel],
     fysiskeBehov: [FysiskBehov.Arbeidsstilling],
     arbeidsmiljøBehov: [],
     grunnleggendeBehov: [GrunnleggendeBehov.SkriveNorsk],
