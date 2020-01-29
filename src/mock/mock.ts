fa6f945106de8895d263709e3775cfb2d610c6fb
André Mossige <mossige.a@gmail.com>
2020-01-29 10:07:07 +0100

    Legg til mock av endre og slett

--- src/mock/mock.ts
+++ src/mock/mock.ts
@@ -5,3 +5,5 @@ const basePath = '/finn-kandidat-api';
 
 fetchMock.get(`${basePath}/kandidater/${etFnr}`, enKandidat);
 fetchMock.post(`${basePath}/kandidater`, [201, enKandidat]);
+fetchMock.put(`${basePath}/kandidater`, enKandidat);
+fetchMock.delete(`${basePath}/kandidater/${etFnr}`, 200);
