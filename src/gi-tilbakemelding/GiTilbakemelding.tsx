97129ef2d1ce952f8b35cc859912ae242c50bb3e
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2020-01-31 12:18:33 +0100

    Endre til ekspanderbart panel

--- src/gi-tilbakemelding/GiTilbakemelding.tsx
+++ src/gi-tilbakemelding/GiTilbakemelding.tsx
@@ -1,73 +1,84 @@
 import React, { FunctionComponent, useState, ChangeEvent } from 'react';
-import { Flatknapp, Knapp } from 'nav-frontend-knapper';
-import NavFrontendChevron from 'nav-frontend-chevron';
-import { Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Knapp } from 'nav-frontend-knapper';
+import { Normaltekst } from 'nav-frontend-typografi';
 import { Textarea } from 'nav-frontend-skjema';
-import './GiTilbakemelding.less';
 import { Tilbakemeldingstatus, Status } from '../api/RestKandidat';
+import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
+import './GiTilbakemelding.less';
+import { AlertStripeSuksess } from 'nav-frontend-alertstriper';
 
 const GiTilbakemelding: FunctionComponent = () => {
-    const [erEkspandert, setEkspandert] = useState<boolean>(false);
     const [tilbakemelding, setTilbakemelding] = useState<string>('');
     const [status, setStatus] = useState<Tilbakemeldingstatus>(Status.IkkeLastet);
     const [feilmelding, setFeilmelding] = useState<string | null>(null);
-
-    const toggle = () => {
-        setEkspandert(!erEkspandert);
-    };
+    const [harTrykketSend, setHarTrykketSend] = useState<boolean>(false);
 
     const onTilbakemeldingChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-        setTilbakemelding(event.target.value);
+        const input = event.target.value;
+
+        setTilbakemelding(input);
+
+        if (harTrykketSend) {
+            validerTilbakemelding(input);
+        }
     };
 
-    const sendForslag = () => {
+    const validerTilbakemelding = (tilbakemelding: string): boolean => {
         if (tilbakemelding.length === 0) {
             setFeilmelding('Vennligst fyll ut feltet.');
-        } else if (tilbakemelding.length < 5) {
+            return false;
+        } else if (tilbakemelding.length < 10) {
             setFeilmelding('Forslaget er for kort.');
-        } else {
+            return false;
+        }
+
+        setFeilmelding(null);
+        return true;
+    };
+
+    const validerOgSendForslag = () => {
+        setHarTrykketSend(true);
+
+        if (validerTilbakemelding(tilbakemelding)) {
             setStatus(Status.LasterInn);
             console.warn('Sender inn tilbakemelding:', tilbakemelding);
         }
     };
 
     return (
-        <div className="gi-tilbakemelding">
-            {erEkspandert && (
+        <Ekspanderbartpanel
+            border
+            tittel="Savner du et alternativ?"
+            tittelProps="element"
+            className="gi-tilbakemelding blokk-m"
+        >
+            {status !== Status.Suksess ? (
                 <>
-                    <Undertittel className="blokk-xxs">Savner du et alternativ?</Undertittel>
                     <Normaltekst className="blokk-s">
                         Send oss et forslag. Forslaget blir kun brukt til videreutvikling av
                         verktøyet.
                     </Normaltekst>
                     <Textarea
                         label="Forslag"
-                        textareaClass="gi-tilbakemelding__textarea blokk-s"
                         feil={feilmelding}
                         value={tilbakemelding}
                         onChange={onTilbakemeldingChange}
                     />
                     <Knapp
                         mini
+                        className="gi-tilbakemelding__sendknapp"
                         htmlType="button"
-                        className="blokk-s"
-                        onClick={sendForslag}
+                        disabled={Boolean(feilmelding)}
+                        onClick={validerOgSendForslag}
                         spinner={status === Status.LasterInn}
                     >
                         Send forslag
                     </Knapp>
                 </>
+            ) : (
+                <AlertStripeSuksess>Takk for din tilbakemelding!</AlertStripeSuksess>
             )}
-            <Flatknapp
-                mini
-                htmlType="button"
-                className="gi-tilbakemelding__toggleknapp"
-                onClick={toggle}
-            >
-                {erEkspandert ? 'Lukk' : 'Savner du et alternativ?'}
-                <NavFrontendChevron type={erEkspandert ? 'opp' : 'ned'} />
-            </Flatknapp>
-        </div>
+        </Ekspanderbartpanel>
     );
 };
 
