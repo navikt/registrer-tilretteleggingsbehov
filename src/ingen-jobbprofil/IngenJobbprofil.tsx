4bcb62b0fd59c870c04deb455245457eddad787d
Kjetil Svalestuen <kjesvale@users.noreply.github.com>
2020-02-03 14:12:38 +0100

    Gjør advarselen ved ingen jobbprofil liten (#22)

--- src/ingen-jobbprofil/IngenJobbprofil.tsx
+++ src/ingen-jobbprofil/IngenJobbprofil.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from 'react';
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper';
 import { Jobbprofilstatus, Status } from '../api/RestKandidat';
+import Ikon from 'nav-frontend-ikoner-assets';
+import { Element } from 'nav-frontend-typografi';
+import './IngenJobbprofil.less';
 
 interface Props {
     status: Jobbprofilstatus;
@@ -8,9 +10,12 @@ interface Props {
 
 const IngenJobbprofil: FunctionComponent<Props> = ({ status }) =>
     status === Status.IkkeFunnet ? (
-        <AlertStripeAdvarsel>
-            Brukeren har ikke jobbprofil, og vil derfor ikke være synlig i kandidatsøket.
-        </AlertStripeAdvarsel>
+        <div className="ingen-jobbprofil blokk-s">
+            <Ikon kind="advarsel-sirkel-fyll" size="1.5em" />
+            <Element>
+                Brukeren har ikke jobbprofil, og vil derfor ikke være synlig i kandidatsøket.
+            </Element>
+        </div>
     ) : null;
 
 export default IngenJobbprofil;
