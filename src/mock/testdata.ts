440bdf7d125e21bdd3b94dc2217023e5dac85838
André Mossige <mossige.a@gmail.com>
2021-01-18 08:49:27 +0100

    Fjern referanser til felt som ikke finnes lengre

--- src/mock/testdata.ts
+++ src/mock/testdata.ts
@@ -9,7 +9,6 @@ export const enKandidat: Kandidat = {
     fnr: etFnr,
     sistEndretAvVeileder: '2019-09-22T20:38:53.312Z',
     sistEndretAv: 'J337738',
-    navKontor: '1001',
     arbeidstid: [Arbeidstid.IkkeHeleDager, Arbeidstid.Fleksibel],
     fysisk: [Fysisk.Ergonomi, Fysisk.Arbeidsstilling, Fysisk.UniversellUtforming],
     arbeidshverdagen: [Arbeidshverdagen.PersonligBistand],
