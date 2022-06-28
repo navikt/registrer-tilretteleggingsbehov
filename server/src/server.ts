c6d0b3b94aae7a3b2e01f087cb5bc572b1735de8
Kjetil Svalestuen <kjetil.svalestuen@nav.no>
2022-06-28 14:40:00 +0200

    G-zip responser

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import compression from 'compression';
 
 const PORT = 3000;
 const BASE_PATH = '/registrer-tilretteleggingsbehov';
@@ -8,23 +9,10 @@ const buildPath = path.join(__dirname, '../build');
 const app = express();
 
 const startServer = () => {
-    app.use('/*', (req, res, next) => {
-        const { origin } = req.headers;
+    app.use(compression());
+    app.use('/*', configureCors);
 
-        if (origin?.includes('intern.nav.no')) {
-            res.header('Access-Control-Allow-Origin', origin);
-        }
-
-        next();
-    });
-
-    app.use(
-        `${BASE_PATH}/static`,
-        express.static(`${buildPath}/static`, {
-            immutable: true,
-            maxAge: 365000000,
-        })
-    );
+    app.use(`${BASE_PATH}/static`, express.static(`${buildPath}/static`, cacheForever));
 
     app.use(`${BASE_PATH}/asset-manifest.json`, express.static(`${buildPath}/asset-manifest.json`));
 
@@ -34,6 +22,21 @@ const startServer = () => {
     app.listen(PORT, () => {
         console.log('Server kjører på port', PORT);
     });
+};
+
+const configureCors = (req: Request, res: Response, next: NextFunction) => {
+    const { origin } = req.headers;
+
+    if (origin?.includes('intern.nav.no')) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
+    next();
+};
+
+const cacheForever = {
+    immutable: true,
+    maxAge: 365000000,
 };
 
 startServer();
